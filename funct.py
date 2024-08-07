ea52311156aa6aa9a31e70b35b1e939602946744
Hawstyyy <hawstyyy@proton.me>
2024-08-07 14:34:53 -0400

    password generator bulk done

---
+++ funct.py
@@ -0,0 +1,22 @@
+import string
+import random as r
+
+class gerador():
+  def gerarSenha(largura):
+    random = []
+    senha = []
+    senha_str = ''
+
+    random += string.ascii_lowercase
+    random += string.ascii_uppercase
+    random += string.digits
+    random += string.punctuation
+
+    for i in range(largura):
+      randomcaracter = r.choice(random)
+
+      senha.append(randomcaracter)
+
+    for i in senha:
+      senha_str += i
+    print(f"- Sua nova senha: {senha_str}")
--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+from funct import gerador
 
+while True:
+  try:
+    choice = int(input("""
++------------------------+
+|   1 - Gerar senha      |
+|   2 - Sair             |
++------------------------+
+  - """))
+  except ValueError:
+    print("\n- Escolha invalida, tente novamente")
+  if choice == 1:
+    largura = int(input("- Insira a quantidade de caracteres que a senha deve ter: "))
+    gerador.gerarSenha(largura)
